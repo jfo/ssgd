1fb5aa8ace08c6a4967eff85786ae83739cfdc3d
Jeff Fowler <jeffowler@gmail.com>
2021-03-28 19:34:36 +0200

    basic markdown

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-import { ensureDirSync } from "https://deno.land/std@0.91.0/fs/mod.ts"
+import { ensureDirSync } from "https://deno.land/std@0.91.0/fs/mod.ts";
+import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 import { assert } from "https://deno.land/std@0.91.0/testing/asserts.ts";
 
 // Everything is syncronous, we read in the file:
-const input = Deno.readTextFileSync('./posts/1.html')
-
+const input = Deno.readTextFileSync("./posts/1.md");
 // We create the output directory:
-ensureDirSync('./build')
+ensureDirSync("./build");
 
-// We write the file unchanged:
-Deno.writeTextFileSync('./build/1.html', input)
-
-// We read in the output and assert it matches the input:
-const output = Deno.readTextFileSync('./build/1.html')
-assert(output === input)
+// We write the file processed through markdown:
+const markup = Marked.parse(input);
+Deno.writeTextFileSync("./build/1.html", markup.content);

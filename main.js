ac23bf72ff83cd71a1743420d235cd9a28d9454d
Jeff Fowler <jeffowler@gmail.com>
2021-12-06 22:54:22 +0100

    opts

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-import {
-  ensureDirSync,
-  expandGlobSync,
-} from "https://deno.land/std/fs/mod.ts";
-import { paramCase } from "https://deno.land/x/case/mod.ts";
+import { ensureDirSync, expandGlobSync } from "https://deno.land/std/fs/mod.ts";
+import { paramCase, normalCase } from "https://deno.land/x/case/mod.ts";
 import { Marked } from "https://deno.land/x/markdown/mod.ts";
 
-// We create the output directory:
-ensureDirSync("./build");
+const options = {
+  outputDir: "build",
+  sourceDir: "src",
+};
 
-// We recursively search for markdown files
-const inputFiles = expandGlobSync(`${Deno.cwd()}/**/*.md`);
+const outputDir = `${Deno.cwd()}/${options.outputDir}`;
+const sourceDir = `${Deno.cwd()}/${options.sourceDir}`;
+
+ensureDirSync(outputDir);
+
+const inputFiles = expandGlobSync(`${sourceDir}/**/*.md`);
 
 for (const file of inputFiles) {
-  // Everything is synchronous, we read in the file:
   const input = Deno.readTextFileSync(file.path);
-
-  // We write the file processed through markdown:
   const markup = Marked.parse(input);
+
   Deno.writeTextFileSync(
-    `./build/${paramCase(markup.meta.title)}.html`,
-    markup.content,
+    `${outputDir}/${paramCase(markup.meta.title)}.html`,
+    markup.content
   );
 }

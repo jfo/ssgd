ae2ef13d71cc5e412e14682114c9931e8c772f67
Jeff Fowler <jeffowler@gmail.com>
2021-12-07 02:32:58 +0100

    hljs

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 import { ensureDirSync, expandGlobSync } from "https://deno.land/std/fs/mod.ts";
 import { paramCase, normalCase } from "https://deno.land/x/case/mod.ts";
+import { parse } from "https://deno.land/std/path/mod.ts";
 import { Marked } from "https://deno.land/x/markdown/mod.ts";
+import hljs from "https://dev.jspm.io/highlightjs";
+
+Marked.setOptions({
+  highlight: (code, lang) => hljs.highlight(lang, code).value,
+});
 
 const options = {
   outputDir: "build",
-  sourceDir: "src",
+  sourceDir: "posts",
 };
 
-const outputDir = `${Deno.cwd()}/${options.outputDir}`;
-const sourceDir = `${Deno.cwd()}/${options.sourceDir}`;
+ensureDirSync(options.outputDir);
 
-ensureDirSync(outputDir);
-
-const inputFiles = expandGlobSync(`${sourceDir}/**/*.md`);
+const inputFiles = expandGlobSync(`${options.sourceDir}/**/*.md`);
 
 for (const file of inputFiles) {
   const input = Deno.readTextFileSync(file.path);
   const markup = Marked.parse(input);
+  const title = markup?.meta?.title || parse(file.name).name;
 
   Deno.writeTextFileSync(
-    `${outputDir}/${paramCase(markup.meta.title)}.html`,
+    `${options.outputDir}/${paramCase(title)}.html`,
     markup.content
   );
 }

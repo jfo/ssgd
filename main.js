e00006d013307c7991525305da460b5c75356f42
Jeff Fowler <jeffowler@gmail.com>
2022-06-30 17:13:34 +0200

    Default and sample, user conf out
    This pulls my specific user options into a json file parsed from the blog's root and introduces a "sample site" that doubles as default templates for use later on. Running deno -A ../main.js from the sample-site directory and then serving the resulting build directory with something like $ python -m SimpleHTTPServer 80 should display a very minimal post/archive on localhost port 80.

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ import { compilePosts, renderArchive, renderPosts, renderRss } from "./lib.js";
 import { ensureDirSync } from "https://deno.land/std/fs/mod.ts";
 import { copySync } from "https://deno.land/std@0.145.0/fs/copy.ts";
 
-const options = {
+const defaultOptions = {
   outputDir: "build",
   sourceDir: "posts",
   staticDir: "static",
-  rootTemplate: "templates/root_template.html",
-  archiveTemplate: "templates/archive_template.html",
-  rssTemplate: "templates/rss_template.xml",
+  rootTemplate: "templates/post.mustache",
+  archiveTemplate: "templates/archive.mustache",
+  rssTemplate: "templates/rss.mustache",
   css: "style.css",
-  includeAnalytics: true,
-  assetHost: "https://blog.jfo.click",
-  baseUrl: "https://blog.jfo.click",
-  // includeAnalytics: false,
-  // assetHost: "http://dev.jfo.click:4321",
-  // baseUrl: "http://dev.jfo.click:4321",
+  assetHost: "http://localhost",
+  baseUrl: "http://localhost",
+};
+
+const userOptions = JSON.parse(Deno.readTextFileSync(".blog-config.json"));
+
+const options = {
+  ...defaultOptions,
+  ...userOptions,
 };
 
 ensureDirSync(options.outputDir);
 const posts = compilePosts(options);
-compilePosts(options);
 renderPosts(posts, options);
 renderArchive(posts, options);
 renderRss(posts, options);

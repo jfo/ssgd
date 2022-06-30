813131ee8690301ffcf2573ff81c4a2f2bc89f2d
Jeff Fowler <jeffowler@gmail.com>
2022-06-30 21:34:40 +0200

    server in progress

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 
-function handler(req, meta) {
+function handler(req, _meta) {
   const host = req.headers.get("host");
 
   const re = new RegExp(`${host}(.*)`);

e646547dcc648a60e131ea2f256f277da09790b6
Jeff Fowler <jeffowler@gmail.com>
2022-07-04 11:37:09 +0200

    xml for rss

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const defaultOptions = {
   staticDir: "static",
   postTemplate: "templates/post.mustache",
   archiveTemplate: "templates/archive.mustache",
-  rssTemplate: "templates/rss.mustache",
+  rssTemplate: "templates/rss.mustache.xml",
   css: "style.css",
   assetHost: "http://localhost",
   baseUrl: "http://localhost",

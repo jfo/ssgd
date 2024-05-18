7b054fee6b199854ce86af88d470fa49911a8722
Jeff Fowler <jeffowler@gmail.com>
2024-05-18 18:08:37 +0200

    title etc

--- cmd/build.mjs
+++ cmd/build.mjs
@@ -10,8 +10,8 @@ import { mkdirSync } from "node:fs";
 export function build(options) {
   // mkdirSync(options.outputDir);
   const posts = compilePosts(options);
+  renderPosts(posts, options);
 
-  // renderPosts(posts, options);
   // renderArchive(posts, options);
   // renderRss(posts, options);
   // copySync(options.staticDir, options.outputDir, { overwrite: true });
